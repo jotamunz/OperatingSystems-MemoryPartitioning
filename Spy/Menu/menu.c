a55e0df92245a357dbaefbb576abbad7860ef35f
Ignacio Alvarez Barrantes <nacho.alvarez2001@gmail.com>
2021-10-24 19:30:26 -0600

    Added Spy Program (Finished)

---
+++ Spy/Menu/menu.c
@@ -0,0 +1,89 @@
+#include "menu.h"
+
+void generateHeader()
+{
+    system("clear");
+    printf("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n");
+    printf("┃                        SPY PROGRAM                             ┃\n");
+    printf("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n");
+    printf("\n\n");
+}
+
+void generateMenu(struct MemoryArray *memory, struct ProcessArray *processes)
+{
+    int selection = 0;
+    char trashCollector[10];
+    generateHeader();
+    printf("[1] Check memory state \n\n");
+    printf("[2] Process state \n\n");
+    printf("[3] Exit \n\n");
+    printf("Select an option: ");
+    scanf("%d", &selection);
+
+    switch (selection)
+    {
+    case 1:
+        generateMemoryMenu(memory, processes);
+        break;
+
+    case 2:
+        generateProcessMenu(memory, processes);
+        break;
+
+    case 3:
+        printf("Saliendo...");
+        break;
+
+    default:
+        printf("\nThe value typed doesn´t correspond to an available option \n\n");
+        printf("Press enter to continue\n");
+        char enter = 0;
+        fgets(trashCollector, sizeof(trashCollector), stdin);
+        while (enter != '\r' && enter != '\n')
+        {
+            enter = getchar();
+        }
+        system("clear");
+        generateMenu(memory, processes);
+    }
+}
+
+void generateMemoryMenu(struct MemoryArray *memory, struct ProcessArray *processes)
+{
+    int selection = 0;
+    char trashCollector[10];
+    generateHeader();
+    printf("Type the number of cells per line: ");
+    scanf("%d", &selection);
+    system("clear");
+    generateHeader();
+    printf("CHECKING ON MEMORY STATE\n\n");
+    printMemoryCells(memory, selection);
+    printf("Press enter to return to the main menu...\n");
+    char enter = 0;
+    fgets(trashCollector, sizeof(trashCollector), stdin);
+    while (enter != '\r' && enter != '\n')
+    {
+        enter = getchar();
+    }
+    system("clear");
+    generateMenu(memory, processes);
+}
+
+void generateProcessMenu(struct MemoryArray *memory, struct ProcessArray *processes)
+{
+    int selection = 0;
+    char trashCollector[10];
+    generateHeader();
+    printf("CHECKING ON PROCESSES STATE\n\n");
+    printProcesses(processes);
+    printf("Press enter to return to the main menu...\n");
+    char enter = 0;
+    fgets(trashCollector, sizeof(trashCollector), stdin);
+    while (enter != '\r' && enter != '\n')
+    {
+        enter = getchar();
+    }
+    system("clear");
+    generateMenu(memory, processes);
+}
