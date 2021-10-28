ec6428958bb1686b372aa97fcf196ebdfc0fc1e0
Ignacio Alvarez Barrantes <nacho.alvarez2001@gmail.com>
2021-10-28 16:40:36 -0600

    Added Spy Life Quality Changes

--- Spy/Menu/menu.c
+++ Spy/Menu/menu.c
@@ -13,7 +13,8 @@ void generateMenu(struct MemoryArray *memory, struct ProcessArray *processes)
 {
     generateHeader();
 
-    if (memory == NULL || processes == NULL) {
+    if (memory == NULL || processes == NULL)
+    {
         // Obtain a pointer to both structres in shared memory
         int key = getSharedProcessArrayId(KEYFILEPATH);
         processes = attachSharedProcessArray(key);
@@ -48,6 +49,7 @@ void generateMenu(struct MemoryArray *memory, struct ProcessArray *processes)
 
     case 3:
         printf("\nTerminating program...\n\n");
+        return;
         break;
 
     default:
@@ -67,25 +69,26 @@ void generateMenu(struct MemoryArray *memory, struct ProcessArray *processes)
 void generateMemoryMenu(struct MemoryArray *memory, struct ProcessArray *processes)
 {
     int selection = 0;
-    char trashCollector[10];
     generateHeader();
     printf("Type the number of cells per line: ");
     scanf("%d", &selection);
+    generateMemoryMenuAux(memory, processes, selection);
+}
+
+void generateMemoryMenuAux(struct MemoryArray *memory, struct ProcessArray *processes, int selection)
+{
     system("clear");
     generateHeader();
     printf("CHECKING ON MEMORY STATE\n\n");
     printMemoryCells(memory, selection);
-    printf("Press enter to refresh or c to exit...\n");
+    printf("Press [ENTER] to refresh or any other key to exit...\n");
     char enter = 0;
-    fgets(trashCollector, sizeof(trashCollector), stdin);
-    while (enter != '\r' && enter != '\n')
-    {
-        enter = getchar();
-    }
+    enter = getchar();
+
     system("clear");
     if (enter == '\r' || enter == '\n')
     {
-        generateMemoryMenu(memory, processes);
+        generateMemoryMenuAux(memory, processes, selection);
     }
     generateMenu(memory, processes);
 }
@@ -93,17 +96,13 @@ void generateMemoryMenu(struct MemoryArray *memory, struct ProcessArray *process
 void generateProcessMenu(struct MemoryArray *memory, struct ProcessArray *processes)
 {
     int selection = 0;
-    char trashCollector[10];
     generateHeader();
     printf("CHECKING ON PROCESSES STATE\n\n");
     printProcesses(processes);
-    printf("Press enter to return to the main menu...\n");
+    printf("Press [ENTER] to refresh or any other key to exit...\n");
     char enter = 0;
-    fgets(trashCollector, sizeof(trashCollector), stdin);
-    while (enter != '\r' && enter != '\n')
-    {
-        enter = getchar();
-    }
+    enter = getchar();
+
     system("clear");
     if (enter == '\r' || enter == '\n')
     {
--- Spy/Menu/menu.h
+++ Spy/Menu/menu.h
@@ -6,3 +6,4 @@ void generateMenu(struct MemoryArray *, struct ProcessArray *);
 void generateHeader();
 void generateMemoryMenu(struct MemoryArray *, struct ProcessArray *);
 void generateProcessMenu(struct MemoryArray *, struct ProcessArray *);
+void generateMemoryMenuAux(struct MemoryArray *, struct ProcessArray *, int);
